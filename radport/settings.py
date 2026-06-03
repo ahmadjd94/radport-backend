3b3c3d254aac38faca9cb9fffa24419f054a0e1f
banana <ahmadjd94@gmail.com>
2026-06-03 16:00:53 +0300

    fix minor issue with collect static

--- radport/settings.py
+++ radport/settings.py
@@ -167,7 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Your Project API',
     'DESCRIPTION': 'API documentation for your project',
